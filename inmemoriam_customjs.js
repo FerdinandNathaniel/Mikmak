5224cbaffb8b47b147af61ea81991061285e024e
FerdinandNathaniel <fabiankok@protonmail.com>
2021-09-11 00:21:54 +0200

    added jquery

--- inmemoriam_customjs.js
+++ inmemoriam_customjs.js
@@ -1,2 +1,12 @@
-// Weetad
-
+// function to load jQuery
+function loadjQuery(){
+    var jq = document.createElement('script'); jq.type = 'text/javascript';
+    jq.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+    document.getElementsByTagName('head')[0].appendChild(jq);
+  }
+  
+  if (typeof jQuery == 'undefined') {  
+    loadjQuery();
+  } else {}
+  
+// app code
